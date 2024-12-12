d3e3eba3283c397ba96942ff903f1a0178b86808
tom <tom@TOM-LENOVO>
2024-12-12 12:34:37 -0500

    update

---
+++ SyncfusionSearch/InventoryModelExtensions.cs
@@ -0,0 +1,21 @@
+using System.Text;
+using FieldMRIServices.Model;
+
+namespace FieldMRIServices.SyncfusionSearch;
+
+public static class InventoryModelExtensions
+{
+    public static string ToSearchKeywords(this InventoryModel inventoryModel)
+    {
+        var keywords = new List<string>();
+
+        var normalizationProcess = NormalizeSearch.Default;
+
+        keywords.Add(normalizationProcess(inventoryModel.NameFirstPart));
+        keywords.Add(normalizationProcess(inventoryModel.PinFirstPart) );
+        keywords.Add(normalizationProcess(inventoryModel.StatusFirstPart));
+        keywords.Add(normalizationProcess(inventoryModel.LocationFirstPart));
+        
+        return string.Join(" ", keywords);
+    }
+}
