6bdbf1c7823215a4eb58ee13a430e573223603d4
tom <tom@TOM-LENOVO>
2025-01-23 11:25:55 -0500

    up

--- Comparers/InventoryNumberComparer.cs
+++ Comparers/InventoryNumberComparer.cs
@@ -4,32 +4,32 @@ namespace FieldMRIServices.Comparers;
 
 public class InventoryNumberComparer : IComparer<object>
 {
-    public int Compare(object xObj, object yObj)
+  public int Compare(object xObj, object yObj)
+  {
+    var x = xObj as ISortableModel;
+    var y = yObj as ISortableModel;
+
+    if (x is null && y is null)
     {
-        var x = xObj as ISortableModel;
-        var y = yObj as ISortableModel;
-        
-        if (x is null && y is null)
-        {
-            return 0;
-        }
-
-        if (x is null)
-        {
-            return 1;
-        }
-
-        if (y is null)
-        {
-            return -1;
-        }
-
-        if (int.TryParse(x.InventoryNumber, out var xValue) &&
-            int.TryParse(y.InventoryNumber, out var yValue))
-        {
-            return xValue.CompareTo(yValue);
-        }
-        
-        return string.Compare(x.InventoryNumber, y.InventoryNumber, StringComparison.Ordinal);
+      return 0;
     }
+
+    if (x is null)
+    {
+      return 1;
+    }
+
+    if (y is null)
+    {
+      return -1;
+    }
+
+    if (int.TryParse(x.InventoryNumber, out var xValue) &&
+        int.TryParse(y.InventoryNumber, out var yValue))
+    {
+      return xValue.CompareTo(yValue);
+    }
+
+    return string.Compare(x.InventoryNumber, y.InventoryNumber, StringComparison.Ordinal);
+  }
 }
